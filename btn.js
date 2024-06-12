ab6ed85a5e07e19c13d9b8654c3ee7a99691bb95
ReliableRoseAffairs <164271757+ReliableRoseAffairs@users.noreply.github.com>
2024-06-12 09:01:36 +0100

    Add files via upload

---
+++ btn.js
@@ -0,0 +1,12 @@
+function redirectTocreate() {
+  // Redirect to the desired page
+  window.location.href = "klogin.html";
+}
+function redirectTokuda() {
+  // Redirect to the desired page
+  window.location.href = "klogin.html";
+}
+function redirectTokclone() {
+  // Redirect to the desired page
+  window.location.href = "kclone.html";
+}
